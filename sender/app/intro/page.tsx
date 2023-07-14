786469ce2bade465229dccb216872a35259405fb
Johannes Kvamme <johannes.kvamme@bekk.no>
2023-07-14 15:19:40 +0200

    :sparkles: Add loading text

--- sender/app/intro/page.tsx
+++ sender/app/intro/page.tsx
@@ -1,12 +1,12 @@
 import localFont from "next/font/local";
 
 // Font files can be colocated inside of `pages`
-const myFont = localFont({ src: "./AreaStencil.ttf" });
+export const streetArtFont = localFont({ src: "./AreaStencil.ttf" });
 
 export default function IndexPage() {
   return (
     <main>
-      <h1 className={myFont.className}>Halsnøy Gatekunst</h1>
+      <h1 className={streetArtFont.className}>Halsnøy Gatekunst</h1>
     </main>
   );
 }
--- sender/app/page.tsx
+++ sender/app/page.tsx
@@ -1,9 +1,25 @@
 /* Components */
 import { HeaderScripts } from "./components/Map/HeaderScripts";
 import dynamic from "next/dynamic";
+import { streetArtFont } from "./intro/page";
 //@ts-ignore
 
-const LazyMap = dynamic(() => import("./components/Map/Map"), { ssr: false });
+const LazyMap = dynamic(() => import("./components/Map/Map"), {
+  ssr: false,
+  loading: () => (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <h1 className={streetArtFont.className}>Laster inn</h1>
+    </main>
+  ),
+});
 
 export default function IndexPage() {
   return (
